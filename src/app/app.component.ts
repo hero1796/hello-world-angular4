3d27ce12e47ad5663fe4de50b0d15e666a254ce3
hero1796 <vuthehung17@gmail.com>
2018-01-08 16:36:19 +0700

    [ngSwitch]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component} from '@angular/core';
-import { FavoriteChangedEventArgs } from './favorite/favorite.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	courses = [1, 2];
-	post = {
-		title : "aaaa",
-		isFavorite : false
-	}
-
-	onFavoriteChanged(eventArgs : FavoriteChangedEventArgs) {
-		console.log("Favorite changed : ", eventArgs);
-	}
+	viewModel = "map";
 }
